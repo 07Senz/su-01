22f0fbee5c90145d227bc1318a3fd056d8436b7d
Senz <uhscshanto@gmail.com>
2026-07-14 15:25:35 +0600

    Updated Database

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  // output: 'export',
+
   // Optional: If you have images, you may need to disable image optimization
   // because 'export' mode doesn't support the standard Next.js image server
   images: {
     unoptimized: true,
   },
+
 };
+
 
 export default nextConfig;
